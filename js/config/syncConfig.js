72bc8bfe590bc1231957b9156eba2d8af3916ee3
BrianPG12 <152166821+BrianPG12@users.noreply.github.com>
2026-04-17 11:13:48 +0900

    Enable Firebase cloud sync configuration

--- js/config/syncConfig.js
+++ js/config/syncConfig.js
@@ -1,11 +1,11 @@
 export const syncConfig = {
-  enabled: false,
+  enabled: true,
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyDsdAj6TIyGkaWSNTg9eIv57OOZqqPvdtk",
+    authDomain: "kanaquiz-d0918.firebaseapp.com",
+    projectId: "kanaquiz-d0918",
+    storageBucket: "kanaquiz-d0918.firebasestorage.app",
+    messagingSenderId: "521211743479",
+    appId: "1:521211743479:web:5d912e3058b411d4a9d7f2"
   }
 };
--- script.js
+++ script.js
@@ -1078,14 +1078,14 @@
 
   // js/config/syncConfig.js
   var syncConfig = {
-    enabled: false,
+    enabled: true,
     firebase: {
-      apiKey: "",
-      authDomain: "",
-      projectId: "",
-      storageBucket: "",
-      messagingSenderId: "",
-      appId: ""
+      apiKey: "AIzaSyDsdAj6TIyGkaWSNTg9eIv57OOZqqPvdtk",
+      authDomain: "kanaquiz-d0918.firebaseapp.com",
+      projectId: "kanaquiz-d0918",
+      storageBucket: "kanaquiz-d0918.firebasestorage.app",
+      messagingSenderId: "521211743479",
+      appId: "1:521211743479:web:5d912e3058b411d4a9d7f2"
     }
   };
 
